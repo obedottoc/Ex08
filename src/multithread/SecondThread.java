2f2061cf0f0b08dbde871c8cc8fe842df40753f8
user2 <user2@simlab26>
2019-09-12 23:04:37 +0530

    developed by hariharan

---
+++ src/multithread/SecondThread.java
@@ -0,0 +1,32 @@
+//**developed by HARIHARAN**//
+package multithread;
+
+public class SecondThread extends Thread {
+		
+	public void run()
+	{
+		
+		try
+		{
+			System.out.println("Second thread started...");
+			while(true)
+			{
+				synchronized(MainThread.RandomValue)
+				{
+					if(MainThread.RandomValue%2!=0&&MainThread.RandomValue!=-2)
+					{
+						System.out.println("THE VALUE IS ODD");
+						System.out.println("Answer="+(MainThread.RandomValue*MainThread.RandomValue*MainThread.RandomValue));
+						MainThread.RandomValue=-2;
+					}
+				}
+			
+				Thread.sleep(500);
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+		
+	}
+}
