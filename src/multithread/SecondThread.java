2ee096a86326aceda6e6bf48ed7e701c5ef80485
Faizul <Faizul@172.17.83.58>
2019-09-12 12:16:13 +0530

    MultiThread

---
+++ src/multithread/SecondThread.java
@@ -0,0 +1,34 @@
+/*
+ * Program to perform second thread
+ *  By Faizul
+ *  faizulsmart10@gmail.com
+ */
+package multithread;
+
+public class SecondThread extends Thread {
+
+	public void run()
+	{
+		try
+		{
+			System.out.println("Second thread started...");
+			while(true)
+			{
+				synchronized(MainThread.RandomValue)
+				{
+					if(MainThread.RandomValue%2!=0&&MainThread.RandomValue!=-1)
+					{
+						System.out.println("Value is Odd");
+						System.out.println("Answer="+(MainThread.RandomValue*MainThread.RandomValue*MainThread.RandomValue));
+						MainThread.RandomValue=-1;
+					}
+				}
+			
+				Thread.sleep(1000);
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+	}
+}
