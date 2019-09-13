482574d36f5a27eac8cdc8cd692bfa7ce7b15f7c
user3 <user3@simlab26>
2019-09-13 22:58:48 +0530

    modified for ex08

---
+++ src/multithread/Firstthread.java
@@ -0,0 +1,31 @@
+package multithread;
+
+public class Firstthread extends Thread {
+		
+	public void run()
+	{
+		
+		try
+		{
+			System.out.println("First thread started...");
+			while(true)
+			{
+				synchronized(Mainthread.RandomValue)
+				{
+					if(Mainthread.RandomValue%2==0&&Mainthread.RandomValue!=-1)
+					{
+						System.out.println("Value is even");
+						System.out.println("Answer="+(Mainthread.RandomValue*Mainthread.RandomValue));
+						Mainthread.RandomValue=-1;
+					}
+				}
+			
+				Thread.sleep(2000);
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+		
+	}
+}
---
+++ src/multithread/Mainthread.java
@@ -0,0 +1,44 @@
+package multithread;
+
+import java.util.*;
+
+public class Mainthread {
+	
+	public static Integer RandomValue;
+	
+	public static void main(String[] args) {
+		Firstthread t1;
+		Secondthread t2;
+		Random r;
+		
+			t1=new Firstthread();
+			t2=new Secondthread();
+			
+			r=new Random();
+			RandomValue=-1;
+			
+			t1.start();
+			t2.start();
+			
+			try
+			{
+			while(true)
+			{								
+				synchronized(RandomValue)
+				{
+					if(RandomValue==-1)
+					{
+						RandomValue=r.nextInt(200);
+						System.out.println("Placed a new number "+RandomValue);
+					}
+				}
+				Thread.sleep(4000);
+			}
+			}catch(InterruptedException ex)
+			{
+				System.out.println("Error:"+ex);
+			}
+		
+	}
+
+}
