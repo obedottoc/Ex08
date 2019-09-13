58c302916290ca94661e5f7e80ba3de5be09c16e
user3 <user3@administrator-HP-Pro-3330-MT>
2019-09-13 17:51:04 +0530

    to find Thread

---
+++ src/multithreading/FirstThread.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+import multithreading.MainThread;
+
+public class FirstThread extends Thread {
+		
+	public void run()
+	{
+		
+		try
+		{
+			System.out.println("First thread started...");
+			while(true)
+			{
+				synchronized(MainThread.RandomValue)
+				{
+					if((MainThread.RandomValue%2)==0&&MainThread.RandomValue!=-1)
+					{
+						System.out.println("Value is greater even");
+						System.out.println("Answer="+(MainThread.RandomValue*MainThread.RandomValue));
+						MainThread.RandomValue=-1;
+					}
+				}
+			
+				Thread.sleep(2000);
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+		
+	}
+}
