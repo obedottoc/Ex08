d08281e5b927c958043413dc88522cdcfb120dba
SATHISH KUMAR <SATHISH KUMAR@DESKTOP-SC1E8E5>
2019-09-15 17:16:48 +0530

    created by kaarthikeyan

---
+++ src/multithreading/Firstthread.java
@@ -0,0 +1,32 @@
+package multithreading;
+
+import multithreading.Mainthread;
+
+public class Firstthread extends Thread
+{
+	 public void run()
+	{
+		try
+		{
+			System.out.println("First thread started...");
+			while(true)
+			{
+				synchronized(Mainthread.RandomValue)
+				{
+					if(Mainthread.RandomValue%2==0&&Mainthread.RandomValue!=-1)
+					{
+						System.out.println("Value is even");
+						System.out.println("Square is ="+Math.pow(Mainthread.RandomValue, 2));
+						Mainthread.RandomValue=-1;
+					}
+				}
+			
+				Thread.sleep(1000);
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+	}
+
+}
---
+++ src/multithreading/Secondthread.java
@@ -0,0 +1,31 @@
+package multithreading;
+
+import multithreading.Mainthread;
+
+public class Secondthread extends Thread {
+
+	public void run()
+	{
+		try
+		{
+			System.out.println("Second thread started...");
+			while(true)
+			{
+				synchronized(Mainthread.RandomValue)
+				{
+					if(Mainthread.RandomValue%2!=0&&Mainthread.RandomValue!=-1)
+					{
+						System.out.println("Value is odd");
+						System.out.println("Cube is ="+Math.pow(Mainthread.RandomValue, 3));
+						Mainthread.RandomValue=-1;
+					}
+				}
+			
+				Thread.sleep(1000);
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+	}
+}
