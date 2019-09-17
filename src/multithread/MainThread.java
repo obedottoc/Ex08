0370882f7f5940ee81f60dd100f125c345f28398
user2 <user2@simlab21>
2019-09-17 18:54:29 +0530

    multithreading

---
+++ src/multithread/MainThread.java
@@ -0,0 +1,97 @@
+package multithread;
+
+import java.util.*;
+
+public class MainThread {
+	
+	public static Integer RandomValue;
+	
+	public static void main(String[] args) {
+		A a=new A("one");
+		a.start();
+	}
+}
+class A extends Thread
+{  
+	String tname;
+	Random r;
+	Thread t1,t2;
+	A(String x)
+	{
+		this.tname=x;
+		
+	}
+	public void run()
+	{
+		try
+		{
+			int num=0;
+			r=new Random();
+			num=r.nextInt(100);
+			for(int i=0;i<10;i++)
+			{
+				if(num%2==0)
+				{
+					t1=new Thread(new even(num));
+					t1.start();
+				}
+				else
+				{
+					t2=new Thread(new odd11(num));
+					t2.start();
+				}
+				Thread.sleep(1000);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Exception is"+e);
+		}	
+		catch(Exception a)
+		{
+			System.out.println("Exception is"+a);
+		}
+	}
+}
+class even implements Runnable
+{
+	int x;
+	even(int x)
+	{
+		this.x=x;
+	}
+	public void run()
+	{
+		System.out.println("num is even"+x+"its square is"+(x*x));
+	}}
+class odd11 implements Runnable
+{
+	int x1;
+	public int x;
+	public odd11(int num) {
+		// TODO Auto-generated constructor stub
+	}
+	 void odd1(int x)
+	{
+		this.x=x;
+	}
+	public void run1()
+	{
+		System.out.println("num is odd"+x+"its cube is"+(x*x*x));
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+				
+				
+					
+			
+		
+		
+	}
+	
+
+
