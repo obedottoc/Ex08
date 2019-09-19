1aa64019cbcc80ac4322d602a47b97c7bb558e67
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-19 15:19:29 +0530

    hello

---
+++ src/multithread/FirstThread.java
@@ -0,0 +1,35 @@
+
+package multithread;
+
+public class FirstThread extends Thread {
+		
+	public void run()
+	{
+		
+		try
+		{
+			System.out.println("First thread started...");
+			while(true)
+			{
+				synchronized(MainThread.RandomValue)
+				{
+					if((MainThread.RandomValue%2==0)&&MainThread.RandomValue!=-1)
+					{
+						System.out.println("Value is an even number");
+						System.out.println("Answer="+(MainThread.RandomValue*MainThread.RandomValue));
+						MainThread.RandomValue=-1;
+						System.out.println("Bye BYE......");
+					}
+						
+				}
+			
+				Thread.sleep(500);
+				
+			}
+		}catch(InterruptedException ex)
+		{
+			System.out.println("Error:"+ex);
+		}
+		
+	}  
+}
